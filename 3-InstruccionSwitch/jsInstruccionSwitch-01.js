a3c1d82ccc12f4523c71fac3033ba43e118dd3ab
juampileiva <130657376+juampileiva@users.noreply.github.com>
2023-04-27 15:59:35 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,7 +1,36 @@
-function mostrar()
-{
-	
-	
+function mostrar() {
+  let mes = document.getElementById("txtMes").value;
+
+switch (mes ) {
+    case " Enero":
+        alert(" que comiences bien el año");
+        break;
+    case " Marzo":
+        alert("a clases !!!");
+        break;
+    case   "Julio" :
+        alert("felices vacaciones");
+        break;
+    case "Diciembre":
+        break;
+    alert(" felices fiestas");
+    break;
+    default: 
+    alert(" ese mes no tiene respuesta");
 
 
 }
+}
+// let mes = document.getElementById("txtMes").value;
+
+ // if (mes == " Enero ") {
+  //  alert(" que comiences bien el año");
+  //} else if (mes == " Marzo ") {
+ //   alert("a clases !!!");
+ // } else if (mes == "Julio") {
+ //   aleert("felices vacaciones");
+ // } else if (mes == " Diciembre") {
+ //   alert(" felices fiestas");
+ // } else {
+ //   alert(" ese mes no tiene respuesta");
+  
--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,6 +1,23 @@
 function mostrar()
-{
-	
+
+    let mes = document.getElementById("txtMes").value;
+
+    switch (mes) {
+      case "Enero":
+        alert(" que comiences bien el año");
+        break;
+      case "Marzo":
+        alert("a clases !!!");
+        break;
+      case "Julio":
+        alert("felices vacaciones");
+        break;
+      case "Agosto":
+        alert(" felices fiestas");
+  
+        break;
+      default:
+        alert(" ese mes no tiene respuesta");
 
 
 
